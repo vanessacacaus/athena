9d4a74e1ee1a80f1ebe74773f1699539717fa47e
Vanessa Cacau <vanessacacaus@gmail.com>
2019-02-04 18:13:53 -0300

    alterações nas interfaces das paginas inicial, busca, recurso

--- js/array.js
+++ js/array.js
@@ -1,5 +1,227 @@
-var recurso1 = {img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/matematicaMovel/recurso1.png", titulo: "Matematica selvagem", ano: "pré-escola"};
-alert(recurso1.img);
+var recurso1 = {
+	tipo: "android",
+	materia: "matematica",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/matematicaMovel/recurso1.png", 
+	titulo: "Matemática selva",
+	descricao: "matematicaselvasaosifljsdoifhsdjfisdkjfdifjedofjdfoidsfjdfjdoofjdfojfd", 
+	ano: "pré-escola",
+	descritores: "",
+	metadados: ""
+};
+
+var recurso2 = {
+	tipo: "android",
+	materia: "matematica",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/matematicaMovel/recurso2.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso3 = {
+	tipo: "android",
+	materia: "matematica",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/matematicaMovel/recurso3.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso4 = {
+	tipo: "android",
+	materia: "matematica",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/matematicaMovel/recurso4.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso5 = {
+	tipo: "android",
+	materia: "matematica",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/matematicaMovel/recurso5.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso6 = {
+	tipo: "android",
+	materia: "portugues",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/portuguesMovel/recurso1.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso7 = {
+	tipo: "android",
+	materia: "portugues",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/portuguesMovel/recurso2.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso8 = {
+	tipo: "android",
+	materia: "portugues",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/portuguesMovel/recurso3.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso9 = {
+	tipo: "android",
+	materia: "portugues",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/portuguesMovel/recurso4.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso10 = {
+	tipo: "android",
+	materia: "portugues",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/portuguesMovel/recurso5.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso11 = {
+	tipo: "computador",
+	materia: "matematica",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/matematica/recurso1.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso12 = {
+	tipo: "computador",
+	materia: "matematica",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/matematica/recurso2.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso13 = {
+	tipo: "computador",
+	materia: "matematica",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/matematica/recurso3.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso14 = {
+	tipo: "computador",
+	materia: "matematica",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/matematica/recurso4.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso15 = {
+	tipo: "computador",
+	materia: "matematica",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/matematica/recurso5.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso16 = {
+	tipo: "computador",
+	materia: "portugues",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/portugues/recurso1.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso17 = {
+	tipo: "computador",
+	materia: "portugues",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/portugues/recurso2.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso18 = {
+	tipo: "computador",
+	materia: "portugues",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/portugues/recurso3.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso19 = {
+	tipo: "computador",
+	materia: "portugues",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/portugues/recurso4.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
+var recurso15 = {
+	tipo: "computador",
+	materia: "portugues",
+	img: "file:///C:/Users/vanes/Downloads/novo-athena/img/home/recursos/portugues/recurso5.png", 
+	titulo: "Crianças, matemática", 
+	descricao: "criançasmatematicaahuahdiajdadkdbfasjkadALXXNSCLKZXMKLDJQWOOOWIODJASL",
+	ano: "4º e 5º anos"
+	descritores: "",
+	metadados: ""
+};
+
 document.getElementById("imagemRec").src = recurso1.img;
 document.getElementById("tituloRec").innerHTML = recurso1.titulo;
 document.getElementById("textoRec").innerHTML = recurso1.ano;
+
+document.getElementById("imagemRec2").src = recurso2.img;
+document.getElementById("tituloRec2").innerHTML = recurso2.titulo;
+document.getElementById("textoRec2").innerHTML = recurso2.ano;
