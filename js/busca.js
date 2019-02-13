8aaf491b65d2915a0fea04ea3c651e8852bee947
Vanessa Cacau <vanessacacaus@gmail.com>
2019-02-13 20:50:56 -0300

    pagina recursos

--- js/busca.js
+++ js/busca.js
@@ -95,5 +95,5 @@ function processData(textoParam){
 
 
 function irPararecurso(n) {
-	location.href="../recursocomputador.html?n=" + recursos[n].identidade;
+	location.href="../novo-athena/recursocomputador.html?n=" + recursos[n].identidade;
 }
