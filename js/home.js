b98975c44896a775157136c22aa334d985f3723a
Vanessa Cacau <vanessacacaus@gmail.com>
2019-02-14 01:23:07 -0300

    barra de rolagem horizontal hidden

--- js/home.js
+++ js/home.js
@@ -1,6 +1,24 @@
+//    alert($("div").scrollTop() + " px");
+
+
+
+/*if($("#randroidcontainer").scrollLeft() < 1){
+	document.getElementById('botaosliderEsquerda').style.visibility = "hidden";
+}
+if($("#randroidcontainer").scrollLeft() > 1){
+	document.getElementById('botaosliderEsquerda').style.visibility = "visible";
+}
+
+if($("#randroidcontainer").scrollLeft() > 1450){
+	document.getElementById('botaosliderDireita').style.visibility = "hidden";
+}*/
+
 function scrollToRight(a){
 	$(a).animate({scrollLeft:'+=200'}, 500);
+	//alert($("#randroidcontainer").scrollLeft());
 }
+
 function scrollToLeft(a){
 	$(a).animate({scrollLeft:'-=200'}, 500);
+	//alert($("#randroidcontainer").scrollLeft());
 }
