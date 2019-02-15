794d2c9271969c956c7ddafbdd0cd833135ecbd0
Vanessa Cacau <vanessacacaus@gmail.com>
2019-02-14 22:49:12 -0300

    home.js

--- js/home.js
+++ js/home.js
@@ -1,24 +1,41 @@
-//    alert($("div").scrollTop() + " px");
+	/*if($("#randroidcontainer").scrollLeft() <= 1){
+		document.getElementById('botaosliderEsquerda').style.visibility = "hidden";
+	}
 
+	if($("#randroidcontainer").scrollLeft() >= 1453){
+		document.getElementById('botaosliderDireita').style.visibility = "hidden";
+	}
 
+	if($("#randroidcontainer").scrollLeft() >= 1){
+		document.getElementById('botaosliderEsquerda').style.visibility = "visible";
+	}
 
-/*if($("#randroidcontainer").scrollLeft() < 1){
-	document.getElementById('botaosliderEsquerda').style.visibility = "hidden";
-}
-if($("#randroidcontainer").scrollLeft() > 1){
-	document.getElementById('botaosliderEsquerda').style.visibility = "visible";
-}
-
-if($("#randroidcontainer").scrollLeft() > 1450){
-	document.getElementById('botaosliderDireita').style.visibility = "hidden";
-}*/
+	if($("#randroidcontainer").scrollLeft() <= 1453){
+		document.getElementById('botaosliderDireita').style.visibility = "visible";
+	}*/
 
 function scrollToRight(a){
 	$(a).animate({scrollLeft:'+=200'}, 500);
-	//alert($("#randroidcontainer").scrollLeft());
+
+	/*if($("#randroidcontainer").scrollLeft() >= 1){
+		document.getElementById('botaosliderEsquerda').style.visibility = "visible";
+	}
+
+	if($("#randroidcontainer").scrollLeft() >= 1453){
+		document.getElementById('botaosliderDireita').style.visibility = "hidden";
+	}
+	alert($("#randroidcontainer").scrollLeft());*/
 }
 
 function scrollToLeft(a){
 	$(a).animate({scrollLeft:'-=200'}, 500);
-	//alert($("#randroidcontainer").scrollLeft());
+/*
+	if($("#randroidcontainer").scrollLeft() <= 1453){
+		document.getElementById('botaosliderDireita').style.visibility = "visible";
+	}
+
+	if($("#randroidcontainer").scrollLeft() <= 1){
+		document.getElementById('botaosliderEsquerda').style.visibility = "hidden";
+	}
+	alert($("#randroidcontainer").scrollLeft());*/
 }
