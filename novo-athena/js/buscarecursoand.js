21e8c19f8da24d7feffa7654e0d371f2431f4e6e
Vanessa Cacau <vanessacacaus@gmail.com>
2019-03-07 16:54:59 -0300

    busca android

---
+++ novo-athena/js/buscarecursoand.js
@@ -0,0 +1,159 @@
+var textoRecursosI = [];
+
+//recursos de portugues
+    var portI1 = {texto: "D1 - Localizar informações explícitas em um texto", descritor:"DPI01"};
+textoRecursosI.push(portI1);
+    var portI2 = {texto: "D2 - Estabelecer relações entre partes de um texto, identificando repetições ou substituições que contribuem para a continuidade de um texto", descritor:"DPI02"};
+textoRecursosI.push(portI2);
+    var portI3 = {texto: "D3 - Inferir o sentido de uma palavra ou expressão", descritor:"DPI03"};
+textoRecursosI.push(portI3);
+    var portI4 = {texto: "D4 - Inferir uma informação implícita em um texto", descritor:"DPI04"};
+textoRecursosI.push(portI4);
+    var portI5 = {texto: "D5 - Interpretar texto com auxílio de material gráfico diverso (propagandas, quadrinhos, foto etc.)", descritor:"DPI05"};
+textoRecursosI.push(portI5);
+    var portI6 = {texto: "D6 - Identificar o tema de um texto", descritor:"DPI06"}; 
+textoRecursosI.push(portI6);
+    var portI7 = {texto: "D7 - Identificar o conflito gerador do enredo e os elementos que constroem a narrativa", descritor:"DPI07"};
+textoRecursosI.push(portI7);
+    var portI8 = {texto: "D8 - Estabelecer relação causa/conseqüência entre partes e elementos do texto", descritor:"DPI08"};
+textoRecursosI.push(portI8);
+    var portI9 = {texto: "D9 - Identificar a finalidade de textos de diferentes gêneros", descritor:"DPI09"};
+textoRecursosI.push(portI9);
+    var portI10 = {texto: "D10 - Identificar as marcas linguísticas que evidenciam o locutor e o interlocutor de um texto", descritor:"DPI10"};
+textoRecursosI.push(portI10);
+    var portI11 = {texto: "D11 - Distinguir um fato da opinião relativa a esse fato", descritor:"DPI11"};
+textoRecursosI.push(portI11);
+    var portI12 = {texto: "D12 - Estabelecer relações lógico-discursivas presentes no texto, marcadas por conjunções, advérbios etc", descritor:"DPI12"};
+textoRecursosI.push(portI12);
+    var portI13 = {texto: "D13 - Identificar efeitos de ironia ou humor em textos variados", descritor:"DPI13"};
+textoRecursosI.push(portI13);
+    var portI14 = {texto: "D14 - Identificar o efeito de sentido decorrente do uso da pontuação e de outras notações", descritor:"DPI14"};
+textoRecursosI.push(portI14);
+    var portI15 = {texto: "D15 - Reconhecer diferentes formas de tratar uma informação na comparação de textos que tratam do mesmo tema, em função das condições em que ele foi produzido e daquelas em que será recebido", descritor:"DPI15"};
+textoRecursosI.push(portI15);
+
+var textoRecursosII = [];
+    var portII1 = {texto: "D1 - Localizar informações explícitas em um texto", descritor:"DPII01"};
+textoRecursosII.push(portII1);
+    var portII2 = {texto: "D2 - Estabelecer relações entre partes de um texto, identificando repetições ou substituições que contribuem para a continuidade de um texto", descritor:"DPII02"};
+textoRecursosII.push(portII2);
+    var portII3 = {texto: "D3 - Inferir o sentido de uma palavra ou expressão", descritor:"DPII03"};
+textoRecursosII.push(portII3);
+    var portII4 = {texto: "D4 - Inferir uma informação implícita em um texto", descritor:"DPII04"};
+textoRecursosII.push(portII4);
+    var portII5 = {texto: "D5 - Interpretar texto com auxílio de material gráfico diverso (propagandas, quadrinhos, foto etc.)", descritor:"DPII05"};
+textoRecursosII.push(portII5);
+    var portII6 = {texto: "D6 - Identificar o tema de um texto", descritor:"DPII06"};
+textoRecursosII.push(portII6);
+    var portII7 = {texto: "D7 - Identificar a tese de um texto", descritor:"DPII07"};
+textoRecursosII.push(portII7);
+    var portII8 = {texto: "D8 - Estabelecer a relação entre a tese e os argumentos oferecidos para sustentá-lá", descritor:"DPII08"};
+textoRecursosII.push(portII8);
+    var portII9 = {texto: "D9 - Diferenciar as partes principais das secundárias em um texto", descritor:"DPII09"};
+textoRecursosII.push(portII9);
+    var portII10 = {texto: "D10 - Identificar o conflito gerador do enredo e os elementos que constroem a narrativa", descritor:"DPII10"};
+textoRecursosII.push(portII10);
+    var portII11 = {texto: "D11 - Estabelecer relação causa/consequência entre partes e elementos do texto", descritor:"DPII11"};
+textoRecursosII.push(portII11);
+    var portII12 = {texto: "D12 - Identificar a finalidade de textos de diferentes gêneros", descritor:"DPII12"};
+textoRecursosII.push(portII12);
+    var portII13 = {texto: "D13 - Identificar as marcas lingüísticas que evidenciam o locutor e o interlocutor de um texto", descritor:"DPII13"};
+textoRecursosII.push(portII13);
+    var portII14 = {texto: "D14 - Distinguir um fato da opinião relativa a esse fato", descritor:"DPII14"};
+textoRecursosII.push(portII14);
+    var portII15 = {texto: "D15 - Estabelecer relações lógico-discursivas presentes no texto, marcadas por conjunções, advérbios etc", descritor:"DPII15"};
+textoRecursosII.push(portII15);
+    var portII16 = {texto: "D16 - Identificar efeitos de ironia ou humor em textos variados", descritor:"DPII16"};
+textoRecursosII.push(portII16);
+    var portII17 = {texto: "D17 - Identificar o efeito de sentido decorrente do uso da pontuação e de outras notações", descritor:"DPII17"};
+textoRecursosII.push(portII17);
+    var portII18 = {texto: "D18 - Reconhecer o efeito de sentido decorrente da escolha de uma determinada palavra ou expressão.", descritor:"DPII18"};
+textoRecursosII.push(portII18);
+    var portII19 = {texto: "D19 - Reconhecer o efeito de sentido decorrente da exploração de recursos ortográficos e/ou morfossintáticos.", descritor:"DPII19"};
+textoRecursosII.push(portII19);
+    var portII20 = {texto: "D20 - Reconhecer diferentes formas de tratar uma informação na comparação de textos que tratam do mesmo tema, em função das condições em que ele foi produzido e daquelas em que será recebido", descritor:"DPII20"};
+textoRecursosII.push(portII20);
+    var portII21 = {texto: "D21 - Reconhecer posições distintas entre duas ou mais opiniões relativas ao mesmo fato ou ao mesmo tema.", descritor:"DPII21"};
+textoRecursosII.push(portII21);
+
+
+var url_string = window.location.href
+var url = new URL(url_string);
+var identidade = url.searchParams.get("n");
+var recursos = [];
+
+if(String(identidade)==""){
+	identidade="null";
+}
+
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "data/recursos_mobile.tsv",
+        dataType: "text",
+        success: function(data) {processData2(data);}
+     });
+});
+
+function processData2(textoParam){
+	var linhas = textoParam.split('\n');
+	for (var i=0; i < linhas.length; i++){
+	    var aux = linhas[i].split('\t');
+	    aux = {identidade:String(aux[0]), nome:String(aux[1]), link:String(aux[2]), tipo:String(aux[3]), autor:String(aux[4]), tamanho:String(aux[5]), android:String(aux[6]), versao:String(aux[7]), permissoes:String(aux[8]), tema:String(aux[9]), descritoresteste:String(aux[10]), descritores:String(aux[11]), idioma:String(aux[12]), atualizacao:String(aux[13]), visualizacao:String(aux[14]), configuracoes:String(aux[15]), multimidia:String(aux[16]), anuncio:String(aux[17]), internet:String(aux[18]), obs:String(aux[19]), miniatura:String(aux[20]), resumo:String(aux[21]), disciplina:String(aux[22]), ano:String(aux[23])};
+	    if(String(identidade)!="null" && String(aux.identidade)!=String(identidade)){
+	    	continue;
+	    }
+	    recursos.push(aux);
+
+		//alert(recursos.length);
+        window.document.title = "Recurso: " + recursos[0].nome;
+		document.getElementById("tituloRecurso").innerHTML = recursos[0].nome;
+		document.getElementById("imagemRecurso").src = recursos[0].miniatura;
+		document.getElementById("textoRecurso").innerHTML = recursos[0].resumo;
+
+		var thread_descritor = recursos[0].descritores.split(';');
+		var tituloDescritoresI = '';
+		var tituloDescritoresII = '';
+		var descritoresI = [];
+		var descritoresII = [];
+		for (var j = 0; j < thread_descritor.length; j++) {
+			for (var k = 0; k < textoRecursosI.length; k++) {
+				//alert(textoRecursosI[k]);
+				if(String(textoRecursosI[k].descritor) != "null" && String(textoRecursosI[k].descritor) != String(thread_descritor[j])){
+					continue;
+				}
+				var auxiliar = textoRecursosI[k].texto;
+			}
+			if(String(auxiliar) != "undefined"){
+				descritoresI.push('<br>'+ auxiliar);
+			}
+
+			for (var k = 0; k < textoRecursosII.length; k++) {
+				//alert(textoRecursos[k]);
+				if(String(textoRecursosII[k].descritor) != "null" && String(textoRecursosII[k].descritor) != String(thread_descritor[j])){
+					//alert(textoRecursosPII[k].texto);
+					continue;
+				}
+			var auxiliar2 = textoRecursosII[k].texto;
+			};
+			if(String(auxiliar2) != "undefined"){
+				descritoresII.push(auxiliar2 + '<br>');
+			}
+
+			if(descritoresI.length > 0){
+				tituloDescritoresI = 'Descritores do 4º a 5º ano';
+			}
+			if(descritoresII.length > 0){
+				tituloDescritoresII = 'Descritores do 8º e 9º ano';
+			}				
+			//alert(thread_descritor[j]);
+			//alert(descritoresPII.length);
+		}
+		document.getElementById("descritoresRecurso").innerHTML = '<em> Ano indicado: ' + recursos[0].ano + 'º ano</em>' + '<b>'+ tituloDescritoresI + '</b>' + descritoresI + '<br>' + '<b>'+ tituloDescritoresII + '</b>' + descritoresII;
+		document.getElementById("metadadosRecurso").innerHTML = '<b> Tema: </b>' + recursos[0].tema + '<br><b> Origem: </b>' + recursos[0].origem + '<br><b> Autor (es): </b>' + recursos[0].autores + '<br> <b> Idioma: </b>' + recursos[0].idioma + '<br> <b> Data da criação:</b>' + recursos[0].datacriacao;
+	}
+}
+
+    function redirecionar() {
+        window.open(recursos[0].linkk, '_blank');
+    }
