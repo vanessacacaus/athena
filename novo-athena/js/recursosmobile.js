b6ee9fabfe02af7c1f7f535a9a2c05e8931744f5
Vanessa Cacau <vanessacacaus@gmail.com>
2019-03-20 08:35:43 -0300

    pagina de recursos android com descricao

---
+++ novo-athena/js/recursosmobile.js
@@ -0,0 +1,251 @@
+var recursos = []; 
+//recursos de matematica
+	var recurso1 = {
+		miniatura: "recursos/img/MatematicaMovel/matematicaparacriancassoma.png", 
+		nome: "Matemática para crianças: Soma",
+		ano: "1",
+		id: "74"
+	};
+	recursos.push(recurso1);
+	 
+	var recurso2 = {
+		miniatura: "recursos/img/MatematicaMovel/adicaoesubcolorido.png", 
+		nome: "Adição e Subtração Colorido",
+		ano: "1",
+		id: "75"
+	};
+	recursos.push(recurso2);
+
+	var recurso3 = {
+		miniatura: "recursos/img/MatematicaMovel/adicaoesubpcriancas.png", 
+		nome: "Adição, subtração para crianças",
+		ano: "1",
+		id: "76"
+	};
+	recursos.push(recurso3);
+
+	var recurso4 = {
+		miniatura: "recursos/img/MatematicaMovel/criancasomatenum.png", 
+		nome: "Crianças, matemática e números",
+		ano: "1",
+		id: "77"
+	};
+	recursos.push(recurso4);
+
+	var recurso5 = {
+		miniatura: "recursos/img/MatematicaMovel/matematicadaselva.png", 
+		nome: "Matemática Selva: Pré-escola",
+		ano: "1",
+		id: "78"
+	};
+	recursos.push(recurso5);
+
+	var recurso6 = {
+		miniatura: "recursos/img/MatematicaMovel/arrumeOperacoes.png", 
+		nome: "Learning Colors Shapes Memory (Cores e formas jogos infantins)",
+		ano: "1",
+		id: "79"
+	};
+	recursos.push(recurso6);
+
+	var recurso7 = {
+		miniatura: "recursos/img/MatematicaMovel/demonstracao_da_area_do_circulo.png", 
+		nome: "Dino Tim : Jogo educativo de formas e números",
+		ano: "2",
+		id: "80"
+	};
+	recursos.push(recurso7);
+
+	var recurso8 = {
+		miniatura: "recursos/img/MatematicaMovel/tabuada9.PNG", 
+		nome: "Monster Numbers (Jogos educativos de Matematica: adição, tabuada)",
+		ano: "2",
+		id: "81"
+	};
+	recursos.push(recurso8);
+
+	var recurso9 = {
+		miniatura: "recursos/img/MatematicaMovel/muroNumerico.png", 
+		nome: "Math Kid (Matemática Crianças)",
+		ano: "2",
+		id: "82"
+	};
+	recursos.push(recurso9);
+
+	var recurso10 = {
+		miniatura: "recursos/img/MatematicaMovel/formas.png", 
+		nome: "Telling Time For Kids (Aprender a Contar o Tempo)",
+		ano: "2",
+		id: "83"
+	};
+	recursos.push(recurso10);
+
+
+//recursos de portugues
+
+	var recurso11 = {
+		miniatura: "recursos/img/PortuguesMovel/palavracantadaoficial.png", 
+		nome: "Palavra Cantada Oficial",
+		ano: "1",
+		id: "1"
+	};
+	recursos.push(recurso11);
+	
+	var recurso12 = {
+		miniatura: "recursos/img/PortuguesMovel/ursinhoaeiou.png", 
+		nome: "Ursinho A E I O U",
+		ano: "1",
+		id: "2"
+	};
+	recursos.push(recurso12);
+
+	var recurso13 = {
+		miniatura: "recursos/img/PortuguesMovel/formapalavras.png", 
+		nome: "Forma Palavras",
+		ano: "1",
+		id: "3"
+	};
+	recursos.push(recurso13);
+
+	var recurso14 = {
+		miniatura: "recursos/img/PortuguesMovel/asminhashistorias.png", 
+		nome: "As minhas histórias",
+		ano: "1",
+		id: "5"
+	};
+	recursos.push(recurso14);
+
+	var recurso15 = {
+		miniatura: "recursos/img/PortuguesMovel/apocadagua.png", 
+		nome: "A poça d'água",
+		ano: "1",
+		id: "6"
+	};
+	recursos.push(recurso15);
+
+	var recurso16 = {
+		miniatura: "recursos/img/PortuguesMovel/viagemespacial.png", 
+		nome: "Formar Palavras - Alfabetização",
+		ano: "2",
+		id: "20"
+	};
+	recursos.push(recurso16);
+
+	var recurso17 = {
+		miniatura: "recursos/img/PortuguesMovel/silaba.png", 
+		nome: "Mundo do Leitor - Histórias",
+		ano: "4",
+		id: "38"
+	};
+	recursos.push(recurso17);
+
+	var recurso18 = {
+		miniatura: "recursos/img/PortuguesMovel/ruthrocha.png", 
+		nome: "O mistério no galinheiro",
+		ano: "3",
+		id: "22"
+	};
+	recursos.push(recurso18);
+
+	var recurso19 = {
+		miniatura: "recursos/img/PortuguesMovel/ortografiapalavrascomsc.png", 
+		nome: "Mundo Leitor - 1950",
+		ano: "4",
+		id: "31"
+	};
+	recursos.push(recurso19);
+
+	var recurso20 = {
+		miniatura: "recursos/img/PortuguesMovel/vanguardas.png", 
+		nome: "Silabando",
+		ano: "2",
+		id: "13"
+	};
+	recursos.push(recurso20);
+
+
+		document.getElementById('recurso1').children[0].src = recursos[0].miniatura;
+		document.getElementById('recurso1').children[1].innerHTML = recursos[0].nome;
+		document.getElementById('recurso1').children[2].innerHTML = recursos[0].ano+"º ano";
+
+		document.getElementById('recurso2').children[0].src = recursos[1].miniatura;
+		document.getElementById('recurso2').children[1].innerHTML = recursos[1].nome;
+		document.getElementById('recurso2').children[2].innerHTML = recursos[1].ano+"º ano";
+
+		document.getElementById('recurso3').children[0].src = recursos[2].miniatura;
+		document.getElementById('recurso3').children[1].innerHTML = recursos[2].nome;
+		document.getElementById('recurso3').children[2].innerHTML = recursos[2].ano+"º ano";
+
+		document.getElementById('recurso4').children[0].src = recursos[3].miniatura;
+		document.getElementById('recurso4').children[1].innerHTML = recursos[3].nome;
+		document.getElementById('recurso4').children[2].innerHTML = recursos[3].ano+"º ano";
+
+		document.getElementById('recurso5').children[0].src = recursos[4].miniatura;
+		document.getElementById('recurso5').children[1].innerHTML = recursos[4].nome;
+		document.getElementById('recurso5').children[2].innerHTML = recursos[4].ano+"º ano";
+
+		document.getElementById('recurso6').children[0].src = recursos[5].miniatura;
+		document.getElementById('recurso6').children[1].innerHTML = recursos[5].nome;
+		document.getElementById('recurso6').children[2].innerHTML = recursos[5].ano+"º ano";
+
+		document.getElementById('recurso7').children[0].src = recursos[6].miniatura;
+		document.getElementById('recurso7').children[1].innerHTML = recursos[6].nome;
+		document.getElementById('recurso7').children[2].innerHTML = recursos[6].ano+"º ano";
+
+		document.getElementById('recurso8').children[0].src = recursos[7].miniatura;
+		document.getElementById('recurso8').children[1].innerHTML = recursos[7].nome;
+		document.getElementById('recurso8').children[2].innerHTML = recursos[7].ano+"º ano";		
+
+		document.getElementById('recurso9').children[0].src = recursos[8].miniatura;
+		document.getElementById('recurso9').children[1].innerHTML = recursos[8].nome;
+		document.getElementById('recurso9').children[2].innerHTML = recursos[8].ano+"º ano";
+
+		document.getElementById('recurso10').children[0].src = recursos[9].miniatura;
+		document.getElementById('recurso10').children[1].innerHTML = recursos[9].nome;
+		document.getElementById('recurso10').children[2].innerHTML = recursos[9].ano+"º ano";	
+
+
+//lingua portuguesa
+		document.getElementById('recurso11').children[0].src = recursos[10].miniatura;
+		document.getElementById('recurso11').children[1].innerHTML = recursos[10].nome;
+		document.getElementById('recurso11').children[2].innerHTML = recursos[10].ano+"º ano";
+
+		document.getElementById('recurso12').children[0].src = recursos[11].miniatura;
+		document.getElementById('recurso12').children[1].innerHTML = recursos[11].nome;
+		document.getElementById('recurso12').children[2].innerHTML = recursos[11].ano+"º ano";
+
+		document.getElementById('recurso13').children[0].src = recursos[12].miniatura;
+		document.getElementById('recurso13').children[1].innerHTML = recursos[12].nome;
+		document.getElementById('recurso13').children[2].innerHTML = recursos[12].ano+"º ano";
+
+		document.getElementById('recurso14').children[0].src = recursos[13].miniatura;
+		document.getElementById('recurso14').children[1].innerHTML = recursos[13].nome;
+		document.getElementById('recurso14').children[2].innerHTML = recursos[13].ano+"º ano";
+
+		document.getElementById('recurso15').children[0].src = recursos[14].miniatura;
+		document.getElementById('recurso15').children[1].innerHTML = recursos[14].nome;
+		document.getElementById('recurso15').children[2].innerHTML = recursos[14].ano+"º ano";
+
+		document.getElementById('recurso16').children[0].src = recursos[15].miniatura;
+		document.getElementById('recurso16').children[1].innerHTML = recursos[15].nome;
+		document.getElementById('recurso16').children[2].innerHTML = recursos[15].ano+"º ano";
+
+		document.getElementById('recurso17').children[0].src = recursos[16].miniatura;
+		document.getElementById('recurso17').children[1].innerHTML = recursos[16].nome;
+		document.getElementById('recurso17').children[2].innerHTML = recursos[16].ano+"º ano";
+
+		document.getElementById('recurso18').children[0].src = recursos[17].miniatura;
+		document.getElementById('recurso18').children[1].innerHTML = recursos[17].nome;
+		document.getElementById('recurso18').children[2].innerHTML = recursos[17].ano+"º ano";		
+
+		document.getElementById('recurso19').children[0].src = recursos[18].miniatura;
+		document.getElementById('recurso19').children[1].innerHTML = recursos[18].nome;
+		document.getElementById('recurso19').children[2].innerHTML = recursos[18].ano+"º ano";
+
+		document.getElementById('recurso20').children[0].src = recursos[19].miniatura;
+		document.getElementById('recurso20').children[1].innerHTML = recursos[19].nome;
+		document.getElementById('recurso20').children[2].innerHTML = recursos[19].ano+"º ano";	
+		
+		function pageAnd(n) {
+			location.href="../novo-athena/recursomobile.html?n=" + recursos[n].id;
+		}
